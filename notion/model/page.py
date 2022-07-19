7fc1cadaa440d3203773cdf2bd648d0c55d352f9
Florian Schäfer <florian.joh.schaefer@gmail.com>
2022-07-19 07:42:58 +0200

    Make page title property name dynamic

--- notion/model/page.py
+++ notion/model/page.py
@@ -4,27 +4,49 @@ from notion.model.block import ChildrenMixin
 from notion.model.common import NotionObjectBase
 
 
+def find_title_property_name(page: "Page") -> str:
+    """Find the name of the title property for a page.
+
+    While every page must have exactly one title, its name can be changed.
+    """
+    for property_name, property_dict in page._data["properties"].items():
+        if property_dict["type"] == "title":
+            return property_name
+
+
 class TitleMixin:
     @property
     def title(self) -> str:
-        return self._data["properties"]["title"]["title"][0]["plain_text"]
+        title_property_name = find_title_property_name(self)
+        return self._data["properties"][title_property_name]["title"][0]["plain_text"]
 
     @title.setter
     def title(self, new_title: str):
+        title_property_name = find_title_property_name(self)
         new_data = self._client.update_page(
             self.id,
-            {"properties": {"title": {"title": [{"text": {"content": new_title}}]}}},
+            {
+                "properties": {
+                    title_property_name: {"title": [{"text": {"content": new_title}}]}
+                }
+            },
         )
         self._data = new_data
 
 
 class Page(NotionObjectBase, ChildrenMixin, TitleMixin):
-    def __init__(self, title: str = None, data=None, client=None):
+    def __init__(
+        self,
+        title: str = None,
+        title_property_name: str = "title",
+        data=None,
+        client=None,
+    ):
         if title:
             data = {
                 "object": "page",
                 "properties": {
-                    "title": {"title": [{"text": {"content": title}}]},
+                    title_property_name: {"title": [{"text": {"content": title}}]},
                 },
             }
         super().__init__(data, client)
