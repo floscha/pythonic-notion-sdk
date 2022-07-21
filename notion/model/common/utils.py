e2641edac1e75ad2b9da0e84b189c054395939e5
Florian Schäfer <florian.joh.schaefer@gmail.com>
2022-07-21 09:27:25 +0200

    Add UUIDv4 class to utils

--- notion/model/common/utils.py
+++ notion/model/common/utils.py
@@ -11,3 +11,11 @@ def is_valid_notion_id(id_str: str) -> bool:
 def parse_notion_datetime(datetime_str: str) -> datetime:
     "Turn a Notion datetime string into a Python `datetime` object."
     return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+
+class UUIDv4(str):
+    def __new__(cls, value):
+        if not is_valid_notion_id(value):
+            raise ValueError(f"{value!r} is not a valid Notion ID.")
+
+        return str.__new__(cls, value)
