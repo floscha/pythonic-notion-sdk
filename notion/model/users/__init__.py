45427d597fac85788969253f129d4a63fc2ca4c3
Florian Schäfer <florian.joh.schaefer@gmail.com>
2022-08-19 22:01:29 +0200

    🐛 Fix import order to solve circular dependecy bug

--- notion/model/users/__init__.py
+++ notion/model/users/__init__.py
@@ -1,5 +1,6 @@
+# Import User class first to avoid circular imports.
+from .user import User  # isort:skip
 from .bot import Bot
 from .person import Person
-from .user import User
 
 __all__ = ["Bot", "Person", "User"]
