c9f5f60a2890eb36525637fb6e4c0374a71c2378
Florian Schäfer <florian.joh.schaefer@gmail.com>
2022-09-18 13:34:08 +0200

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pythonic-notion-sdk",
-    version="0.0.3",
+    version="0.1.0",
     description="A pythonic way to interact with Notion without passing JSON around.",
     author="Florian Schaefer",
     author_email="florian.schaefer@gmail.com",
